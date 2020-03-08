cd1992e87957c1a17c19a5bc03be30253a51f54f
Henry Fan <henry19910403@gmail.com>
2020-03-08 16:52:56 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Henry on 2020/3/8.
+//  Copyright © 2020 Henry Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
